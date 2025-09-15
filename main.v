38b303a80ae52549295291227e4f6af74da1ac86
JoeyShapiro <joeyashapiro@gmail.com>
2025-09-14 21:01:40 -0500

    can store requests

--- main.v
+++ main.v
@@ -1,8 +1,11 @@
 module main
 
 import veb
+import time
+import db.sqlite
+import orm
 
-const port = 8080
+const port = 8081
 
 struct State {
 mut:
@@ -26,21 +29,53 @@ pub fn (app &App) before_request() {
 
 @['/'; get]
 pub fn (app &App) get_footer(mut ctx Context) veb.Result {
-    id := ctx.query['id'] or {
-        // we can exit early and send a different response if no `name` parameter was passed
-        "0"
-    }
+    id := ctx.query['id'] or { "0" }
 
-	println(id)
+	store_request(id, ctx.ip(), ctx.user_agent(), time.now().local_to_utc().format_ss_milli()) or { 
+		eprintln('failed to log request: ${err}')
+	}
     
     return ctx.file('footer.jpeg')
 }
 
+fn store_request(id string, ip string, user_agent string, timestamp string) ! {
+	// more data is nice, but i think this is all i really need
+	mut db := sqlite.connect('requests.db')!
+	mut qb := orm.new_query[Request](db)
+	qb.insert(Request{
+		uid: id
+		ip: ip
+		user_agent: user_agent
+		timestamp: timestamp
+	})!
+	db.close()!
+}
+
 fn main() {
-	println('veb example')
+	// could create file on fail but meh
+	mut db := sqlite.connect('requests.db')!
+	mut qb := orm.new_query[Request](db)
+	qb.create()! // this doesnt seem to recreate if exists
+	qb.insert(Request{
+		uid: 'init'
+		ip: '127.0.0.1'
+		user_agent: 'init'
+		timestamp: time.now().local_to_utc().format_ss_milli()
+	})!
+	db.close()!
+
 	// veb.run(&App{}, port)
 	mut app := &App{}
 	veb.run_at[App, Context](mut app, port: port, family: .ip, timeout_in_seconds: 2) or {
 		panic(err)
 	}
+}
+
+@[table: 'requests']
+struct Request {
+	id         int    @[primary; sql: serial]
+	uid        string
+	ip         string
+	user_agent string
+	timestamp  string
 }
